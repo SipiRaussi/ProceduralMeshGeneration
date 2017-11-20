77ec2f6325839dbeb66735620ab9e6bb31785503
Sipi Raussi <sraussi94@gmail.com>
2017-11-20 16:19:00 +0200

    Let's make cube!

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -16,15 +16,22 @@ public class Grid : MonoBehaviour
 
         //Generate vertices
         vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        Vector2[] uv = new Vector2[vertices.Length];
+        Vector4[] tangents = new Vector4[vertices.Length];
+        Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
         for (int i = 0, y = 0; y <= ySize; y++)
         {
             for (int x = 0; x <= xSize; x++, i++)
             {
                 vertices[i] = new Vector3(x, y);
+                uv[i] = new Vector2((float)x / xSize, (float)y / ySize);
+                tangents[i] = tangent;
             }
         }
 
         mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.tangents = tangents;
 
         //Make triangles
         int[] triangles = new int[xSize * ySize * 6];
@@ -36,8 +43,9 @@ public class Grid : MonoBehaviour
                 triangles[ti + 1] = triangles[ti + 4] = vi + xSize + 1;
                 triangles[ti + 2] = triangles[ti + 3] = vi + 1;
                 triangles[ti + 5] = vi + xSize + 2;
-                mesh.triangles = triangles;
             }
+            mesh.triangles = triangles;
+            mesh.RecalculateNormals();
         }
     }
 
